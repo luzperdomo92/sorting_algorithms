197ec917f9e6f2c058104ce99502ce63e4ca9b92
Luz Perdomo <lilyperdomo92@gmail.com>
2020-06-14 17:21:47 -0500

    added 0-bubble_sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - bubble sort implementation
+ * @array: array of int elements to sort
+ * @size: size of the array
+ * Return: nothing
+ */
+void bubble_sort(int *array, size_t size)
+{
+    size_t i;
+    int ordered = 0;
+    int aux;
+    
+    while (ordered == 0)
+    {
+        ordered = 1;
+        for (i = 0; i < size; i++)
+        {
+            if((i + 1) >= size)
+            {
+                if (array[i + 1] < array[i])
+                {
+                    ordered = 0;
+                    aux =  array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = aux;
+                }
+            }
+        }
+        print_array(array, size);
+    }
+}
