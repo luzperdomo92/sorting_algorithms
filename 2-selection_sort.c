36240f5bd1f4ba99df8cb9f95d33a9afe50d9a4d
Luz Perdomo <lilyperdomo92@gmail.com>
2020-06-15 16:27:36 -0500

    fixed logic of 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,7 +8,7 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j;
+	size_t i, j, min_compare_index;
 	int aux;
 
 	if (!array)
@@ -16,15 +16,19 @@ void selection_sort(int *array, size_t size)
 
 	for (i = 0; i < size; i++)
 	{
+		min_compare_index = i;
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[j] < array[i])
-			{
-				aux =  array[i];
-				array[i] = array[j];
-				array[j] = aux;
-				print_array(array, size);
-			}
+			if (array[j] < array[min_compare_index])
+				min_compare_index = j;
 		}
+		if (min_compare_index != i)
+		{
+			aux =  array[i];
+			array[i] = array[min_compare_index];
+			array[min_compare_index] = aux;
+			print_array(array, size);
+		}
+
 	}
 }
