25e9f2af2af7ca82755afc24c67f34c59eb0c4bf
Luz Perdomo <lilyperdomo92@gmail.com>
2020-06-15 15:51:23 -0500

    fixed swap for 1-insertion_sort_list

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -6,19 +6,23 @@
  * @next: next node
  * Return: nothing
  */
-void swap_nodes(listint_t *prev_node, listint_t *next_node){
+void swap_nodes(listint_t *prev_node, listint_t *next_node, listint_t **list){
     listint_t *before_prev, *after_next;
 
     before_prev = prev_node->prev;
     after_next = next_node->next;
 
     next_node->prev = before_prev;
-    before_prev->next = next_node;
     next_node->next = prev_node;
+    if (before_prev)
+        before_prev->next = next_node;
+    else
+        list = next_node;
 
     prev_node->prev = next_node;
     prev_node->next = after_next;
-    after_next->prev = prev_node;
+    if (after_next)
+        after_next->prev = prev_node;
 }
 
 
@@ -42,14 +46,14 @@ void insertion_sort_list(listint_t **list)
         if (iter->next->n < iter->n)
         {
             aux = iter->next;
-            swap_nodes(iter, iter->next);
+            swap_nodes(iter, iter->next, list);
             print_list(*list);
-            while (aux->prev != NULL && aux->n < aux->prev->n)
+            while (aux->prev && aux->n < aux->prev->n)
             {
-                swap_nodes(aux->prev, aux);
+                swap_nodes(aux->prev, aux, list);
                 print_list(*list);
-                aux = aux->prev;
             }
+            continue;
         }
 
         iter = iter->next;
