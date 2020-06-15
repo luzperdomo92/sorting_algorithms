ef6920f5dbc1e582113a8c34d2c5370e6165e20b
Luz Perdomo <lilyperdomo92@gmail.com>
2020-06-15 12:12:44 -0500

    fixed syles

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,29 +8,29 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i;
-    int ordered = 0;
-    int aux;
+	size_t i;
+	int ordered = 0;
+	int aux;
 
-    if (!array)
+	if (!array)
 		return;
-    
-    while (ordered == 0)
-    {
-        ordered = 1;
-        for (i = 0; i < size; i++)
-        {
-            if((i + 1) < size)
-            {
-                if (array[i + 1] < array[i])
-                {
-                    ordered = 0;
-                    aux =  array[i];
-                    array[i] = array[i + 1];
-                    array[i + 1] = aux;
-                    print_array(array, size);
-                }
-            }
-        }
-    }
+	
+	while (ordered == 0)
+	{
+		ordered = 1;
+		for (i = 0; i < size; i++)
+		{
+			if((i + 1) < size)
+			{
+				if (array[i + 1] < array[i])
+				{
+					ordered = 0;
+					aux =  array[i];
+					array[i] = array[i + 1];
+					array[i + 1] = aux;
+					print_array(array, size);
+				}
+			}
+		}
+	}
 }
